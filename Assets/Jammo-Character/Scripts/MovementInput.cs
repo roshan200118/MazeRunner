ebc4d08a5fce5798b272f2bc1e0bd95a86bd3873
Roshan Nanthapalan <roshan200118@gmail.com>
2022-10-23 23:19:34 -0400

    Possible camera fix
    - Maybe happens bc I move mouse after clicking play?

--- Assets/Jammo-Character/Scripts/MovementInput.cs
+++ Assets/Jammo-Character/Scripts/MovementInput.cs
@@ -40,9 +40,13 @@ public class MovementInput : MonoBehaviour {
 	void Start () {
 		anim = this.GetComponent<Animator> ();
 		cam = Camera.main;
+		cam.transform.position = new Vector3(8.45f, 2.403f, -2.214f);
+
 		controller = this.GetComponent<CharacterController> ();
 		//Lock the mouse cursor to the center of the game window
 		Cursor.lockState = CursorLockMode.Locked;
+
+
 	}
 	
 	// Update is called once per frame
